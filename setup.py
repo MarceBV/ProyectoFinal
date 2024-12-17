86065af52279654d0c2e3f499eb3a0870d1beb58
Marcela Bethancourt <cheluquis29@yahoo.com>
2024-12-16 21:38:08 -0600

    primera subida

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='proyecto final de Product Development',
+    author='Marcela Bethancourt',
+    license='',
+)
